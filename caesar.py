a22f034991464b2e72cfacdc7dbb57658c124328
Gbachant <Gbachant@gmail.com>
2016-12-11 22:58:12 -0500

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -1,54 +1,20 @@
-def alphabet_position(letter):
-    position = ''
-    if letter.upper() == "A":
-        position = 0
-    elif letter.upper() == "B":
-        position = 1
-    elif letter.upper() == "C":
-        position = 2
-    elif letter.upper() == "D":
-        position = 3
-    elif letter.upper() == "E":
-        position = 4
-    elif letter.upper() == "F":
-        position = 5
-    elif letter.upper() == "G":
-        position = 6
-    elif letter.upper() == "H":
-        position = 7
-    elif letter.upper() == "I":
-        position = 8
-    elif letter.upper() == "J":
-        position = 9
-    elif letter.upper() == "K":
-        position = 10
-    elif letter.upper() == "L":
-        position = 11
-    elif letter.upper() == "M":
-        position = 12
-    elif letter.upper() == "N":
-        position = 13
-    elif letter.upper() == "O":
-        position = 14
-    elif letter.upper() == "P":
-        position = 15
-    elif letter.upper() == "Q":
-        position = 16
-    elif letter.upper() == "R":
-        position = 17
-    elif letter.upper() == "S":
-        position = 18
-    elif letter.upper() == "T":
-        position = 19
-    elif letter.upper() == "U":
-        position = 20
-    elif letter.upper() == "V":
-        position = 21
-    elif letter.upper() == "W":
-        position = 22
-    elif letter.upper() == "X":
-        position = 23
-    elif letter.upper() == "Y":
-        position = 24
-    elif letter.upper() == "Z":
-        position = 25
+from helpers import alphabet_position, rotate_character
+from sys import argv, exit
+
+def user_function_is_valid(cl_args):
+    if len(cl_args) == 2 and type(cl_args[1]) is int:
+        return True
+    else:
+        print("usage: python3 caesar.py n")
+        return False
+        exit()
+
+def encrypt():
+    """Switch all characters for new characters a specified distance away"""
+    text = str(input("Type a message: "))
+    rot = int(argv[1])
+    new_text = ""
+    for i in text:
+        new_text += str(rotate_character(i, rot))
+    print(new_text)
+    return new_text
